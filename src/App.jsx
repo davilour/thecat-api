0ea499e03f2b9906ef615e74e583edbdee46c745
USER 2 <davi.lourenco1702@gmail.com>
2023-11-12 21:37:25 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Select from "./Components/Select";
+import Card from "./Components/Card";
+
+function App() {
+    return (
+        <>
+            <Select />
+            <Card />
+        </>
+    );
+}
+
+export default App;
