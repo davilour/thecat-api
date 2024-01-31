5ce09e32ebed84768e1f9609cfb16115ffbc59cc
unknown <davizinholdp17@gmail.com>
2024-01-31 19:57:49 -0300

    improvised dotenv

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,6 @@
+import firebaseConfig from "./firebaseConfig.js";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDvEdGW1YoAg-IqD6eYmlUGhF4-fZhI5MA",
-    authDomain: "thecatapi-880b8.firebaseapp.com",
-    projectId: "thecatapi-880b8",
-    storageBucket: "thecatapi-880b8.appspot.com",
-    messagingSenderId: "1008993878651",
-    appId: "1:1008993878651:web:d7158faf8c592fe3b218de",
-};
 
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
